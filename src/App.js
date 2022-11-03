d673b63c375f9f38f4b654ace9024f285f55b1e3
Nana K <73991180+topboyasante@users.noreply.github.com>
2022-11-03 11:22:21 +0000

    Added React Router, and created Login Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import{Routes, Route} from 'react-router-dom'
+import Login from './Components/Login';
+
+
 function App() {
   return (
     <div className="App">
-     
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+      </Routes>
     </div>
   );
 }
